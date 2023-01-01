e5d36a748952eeb8fdb4f1b4358ce23268f3e6e3
chidera-ovuoba <107512707+chidera-ovuoba@users.noreply.github.com>
2023-01-01 21:30:24 +0100

    1/1/2023 commit

--- vite-project/src/sections/Testimonial.jsx
+++ vite-project/src/sections/Testimonial.jsx
@@ -2,7 +2,14 @@ import React from 'react'
 
 const Testimonial = () => {
   return (
-    <div>Testimonial</div>
+    <section className='bookATrip__section'>
+      <div className='bookATrip__content'>
+      <div className='bookATrip__content_header'>
+      <h4>Easy and Fast</h4>
+      <h2>Book your next trip in 3 easy steps </h2>
+        </div>
+      </div>
+      </section>
   )
 }
 
